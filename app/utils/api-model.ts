d54a266e2a7874f56443362e6982c58ebe293485
Marcos Cordeiro <marcosc974@gmail.com>
2024-12-20 10:54:29 -0300

    fix/api-model

--- app/utils/api-model.ts
+++ app/utils/api-model.ts
@@ -1,12 +1,44 @@
+const api = "https://script.google.com/macros/s/YOUR_KEY/exec";
 export async function getApiData() {
-    try {
-        const api = 'https://script.google.com/YOUR_KEY_HERE'
-        const response = await fetch(api)
-        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`)
-        const data = await response.json()
-        return data
-    } catch (error) {
-        console.error('Error fetching data:', error)
-    }
+  try {
+    const response = await fetch(api);
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 }
 
+export async function setApiData(
+  planilha: any,
+  linha: any,
+  coluna: any,
+  valor: any
+) {
+  const payload = {
+    planilha: planilha,
+    linha: linha,
+    coluna: coluna,
+    valor: valor,
+  };
+
+  try {
+    const response = await fetch(api, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Erro na requisição: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log("Resposta do servidor:", data);
+  } catch (error) {
+    console.error("Erro ao atualizar a planilha:", error);
+  }
+}
